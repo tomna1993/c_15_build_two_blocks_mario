8b0015378fbdd1264ae6c27b5c66395bde612050
TomasNagy <nagy.tamas.guitar@gmail.com>
2023-04-28 20:45:01 +0200

    Add comments

--- mario.c
+++ mario.c
@@ -2,13 +2,18 @@
 #include <cs50.c>
 
 int main(void)
-{
+{	
+	// Prompt user for integer number
 	int height = get_int("Height: ");
 
+	// Nested for loop;
+	// iterate trough rows
 	for(int i = 1; i <= height; i++)
 	{
+		// iterate trough columns
 		for(int j = 0; j < height; j++)
 		{
+			// print " " characters; if we subtract i from height we get the number of "#" in a row
 			if(j < height - i)
 			{
 				printf(" ");
@@ -18,7 +23,7 @@ int main(void)
 				printf("#");
 			}
 		}
-
+		// at the end of the row go to the next row
 		printf("\n");
 	}
 }
